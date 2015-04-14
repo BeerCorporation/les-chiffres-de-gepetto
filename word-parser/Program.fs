7f918904cd94429aea04ab106fd80333e18bd992
Damien Frikha <damien.frikha@outlook.com>
2015-04-14 11:10:15 +0200

    Add text file dictionnary complete with forms

---
+++ word-parser/Program.fs
@@ -0,0 +1,28 @@
+﻿open FSharp.Data
+
+type XmlData = XmlProvider<"Template.xml">
+
+let outFile = new System.IO.StreamWriter "data.txt"
+
+[<EntryPoint>]
+let main _ = 
+    printfn "Parsing data..."
+    let rawXmlData = XmlData.Load "../../Morphalou.xml"
+    
+    printfn "Printing results..."
+    rawXmlData.LexicalEntries 
+    |> Seq.map (fun item -> 
+        List.concat [
+                [item.FormSet.LemmatizedForm.GrammaticalCategory]
+                [item.FormSet.LemmatizedForm.Orthography]
+                [
+                    for inflected in item.FormSet.InflectedForms ->
+                        inflected.Orthography
+                ]
+            ]
+        )
+    |> Seq.map (fun item -> item |> Seq.distinct)
+    |> Seq.map (fun item -> String.concat " | " item)
+    |> Seq.iter (fun item -> item |> outFile.WriteLine)
+    outFile.Close ()
+    0
